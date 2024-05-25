e40343d2e204e1e32bd7acd56f918c3756edb996
Beanie Guy <130616765+BeanieGuy09@users.noreply.github.com>
2024-05-25 23:24:25 +0000

    Chess added (chess.com) added to script.js as well

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const myArray = ["taskWebsite/taskWebsite.html", "taskJog/taskJog.html", "taskSleep/taskSleep.html","taskRice/taskRice.html","taskAutoPets/autoPets.html", "taskShowdown/taskShowdown.html", "taskBlackjack/taskBlackjack.html", "taskTrail/taskTrail.html","taskFNF/taskFNF.html","taskCourt/taskCourt.html","taskDieDungeon/taskDieDungeon.html"];
+const myArray = ["taskWebsite/taskWebsite.html", "taskJog/taskJog.html", "taskSleep/taskSleep.html","taskRice/taskRice.html","taskAutoPets/autoPets.html", "taskShowdown/taskShowdown.html", "taskBlackjack/taskBlackjack.html", "taskTrail/taskTrail.html","taskFNF/taskFNF.html","taskCourt/taskCourt.html","taskDieDungeon/taskDieDungeon.html","taskChess/taskChess.html"];
 
 // Function to pick a random item from the array
 function getRandomSideQuest() {
