58dc6fd54d3768018d0ab0f91321dafc003f3fc3
Beanie Guy <130616765+BeanieGuy09@users.noreply.github.com>
2024-05-25 19:14:50 +0000

    Finished task create website

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const myArray = ["apple", "banana", "orange", "grape", "kiwi"];
+const myArray = ["taskWebsite/taskWebsite.html", "taskJob/taskJog.html", "taskSleep/taskSleep.html"];
 
 // Function to pick a random item from the array
 function getRandomSideQuest() {
     const randomIndex = Math.floor(Math.random() * myArray.length);
     console.log("Randomly chosen item:", myArray[randomIndex]);
-    return myArray[randomIndex];
+    window.location.href= myArray[randomIndex];
 }
 
 // Usage
