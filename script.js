4c8edb646fe38c22e00c92fe2d39c587e4e38fe4
Beanie Guy <130616765+BeanieGuy09@users.noreply.github.com>
2024-05-25 20:47:12 +0000

    Auto Pets Completed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const myArray = ["taskWebsite/taskWebsite.html", "taskJog/taskJog.html", "taskSleep/taskSleep.html","taskRice/taskRice.html"];
+const myArray = ["taskWebsite/taskWebsite.html", "taskJog/taskJog.html", "taskSleep/taskSleep.html","taskRice/taskRice.html","taskAutoPets/autoPets.html"];
 
 // Function to pick a random item from the array
 function getRandomSideQuest() {
